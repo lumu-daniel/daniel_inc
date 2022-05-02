88c8d00e048f27e5669e14a3b63f3f0f81cdabc3
Daniel <lumukd@gmail.com>
2022-05-02 11:43:03 -0500

    fixed links

--- scripts/task_list.js
+++ scripts/task_list.js
@@ -2,15 +2,17 @@ var str = [];
 
 function addTask(){
     x = (document.getElementById("task_text")||{}).value||"";
+    document.getElementById("task_text").value = "";
     if(x!=null){
         str.push(x);
     }
-    localStorage.setItem("Data",str.join("\n"))
-    alert();
-    document.getElementById("area_1").style.display = localStorage.getItem("Data");
+    localStorage.setItem("Data",str.join("\n"));
+    document.getElementById("area_1").value = localStorage.getItem("Data");
 }
 
 function clearTasks(){
     str = [];
     document.getElementById("task_text").style.display = str;
+    localStorage.clear;
+    document.getElementById("area_1").value = "";
 }
