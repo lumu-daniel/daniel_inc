b2f9ad6c40523aa919068a4770a26d5c4bb4e8da
Daniel <lumukd@gmail.com>
2022-05-02 11:34:41 -0500

    failed work

--- scripts/task_list.js
+++ scripts/task_list.js
@@ -1,12 +1,13 @@
 var str = [];
 
 function addTask(){
-    x = (document.getElementById(task_text)||{}).value||"";
+    x = (document.getElementById("task_text")||{}).value||"";
     if(x!=null){
         str.push(x);
     }
     localStorage.setItem("Data",str.join("\n"))
-    document.getElementById("task_text").style.display = localStorage.getItem("Data");
+    alert();
+    document.getElementById("area_1").style.display = localStorage.getItem("Data");
 }
 
 function clearTasks(){
