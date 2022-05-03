e3c67a2128cafd69f92333e3cef0dfb6df2659a0
Daniel <lumukd@gmail.com>
2022-05-03 09:08:41 -0500

    innerhtml

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,7 +1,7 @@
 function calculateTip(){
     let amount = parseFloat(document.getElementById("sub_total").value);
     let percentage = parseFloat(document.getElementById("tip").value)/100;
-    document.getElementById("total_amt").value =  parseFloat(amount*percentage);
+    document.getElementById("total_amt").innerHTML =  parseFloat(amount*percentage);
 }
 
    
