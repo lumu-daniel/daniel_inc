27fe1a43d8780c05c1be36ea19ccd4b8fd1a65b4
Daniel <lumukd@gmail.com>
2022-05-03 08:36:05 -0500

    fixed

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -1,18 +1,7 @@
 function calculateTip(){
     let amount = parseFloat(document.getElementById("sub_total").value);
     let percentage = parseFloat(document.getElementById("tip").value)/100;
-    document.getElementById("total_amt").value =  parseFloat(amount*percentage).toString;
-    trial()
+    document.getElementById("total_amt").value =  parseFloat(amount*percentage);
 }
 
-var myfunc = function(a, x) {
-    return a * x;
-};
-
-function trial(){
-   var x = myfunc(2, 3);
-   var y = myfunc;
-   alert(x);
-   alert(y(2,3));
-}
    
