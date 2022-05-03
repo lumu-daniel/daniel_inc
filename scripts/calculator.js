4a5d45aed14d3ed6f7edd3e5e811aae97c780b86
Daniel <lumukd@gmail.com>
2022-05-03 08:30:57 -0500

    trial

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -2,4 +2,17 @@ function calculateTip(){
     let amount = parseFloat(document.getElementById("sub_total").value);
     let percentage = parseFloat(document.getElementById("tip").value)/100;
     document.getElementById("total_amt").value =  parseFloat(amount*percentage).toString;
+    trial()
 }
+
+var myfunc = function(a, x) {
+    return a * x;
+};
+
+function trial(){
+   var x = myfunc(2, 3);
+   var y = myfunc;
+   alert(x);
+   alert(y(2,3));
+}
+   
